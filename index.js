1ee045a8d0a10c8703b39afac4067e3613f4a290
Julian Aladio <jmaladio@gmail.com>
2025-06-09 21:58:42 -0300

    Feat add base node server

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('Aguante la Ingeniería de Software');
+});
+
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
